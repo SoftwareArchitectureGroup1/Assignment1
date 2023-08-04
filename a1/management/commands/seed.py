873110177c8ec5aef3926ff2e71a9ce7a7b61401
cppizarro <cppizarro@miuandes.cl>
2023-08-04 16:13:24 -0400

    base for seed file

--- a1/management/commands/seed.py
+++ a1/management/commands/seed.py
@@ -1,5 +1,27 @@
 from django.core.management.base import BaseCommand
+from a1.models import *
 
+MODE_CLEAR = 'clear'
 
 class Command(BaseCommand):
-    pass
+    
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.MIGRATE_HEADING('Starting...'))
+
+        if options['mode'] == MODE_CLEAR:
+            self.__clear_data()
+
+    def __clear_data():
+        pass
+
+    def __create_authors():
+        pass
+
+    def __create_books():
+        pass
+
+    def __create_reviews():
+        pass
+
+    def __create_sales():
+        pass
